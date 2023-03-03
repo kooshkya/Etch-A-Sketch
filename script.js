c8bec9cfe586220a4b102ade71c02cb6c0ddd641
Amir Kooshky's WSL <kooshkya@gmail.com>
2023-03-03 08:40:46 +0330

    main: create background color picker

--- script.js
+++ script.js
@@ -5,20 +5,29 @@ const foregroundDefaultColor = "#000000";
 // element declarations:
 let foregroundColorPicker;
 let foregroundColorPickerLabel;
+let backgroundColorPicker;
+let backgroundColorPickerLabel;
 
 
 function findElements() {
     foregroundColorPickerLabel = document.querySelector("#foreground-color-picker-label");
     foregroundColorPicker = document.querySelector("#foreground-color-picker");
+    backgroundColorPickerLabel = document.querySelector("#background-color-picker-label");
+    backgroundColorPicker = document.querySelector("#background-color-picker");
 }
 
 
 function initializePage() {
-    foregroundColorPicker.addEventListener("change", (event) => {
+    foregroundColorPicker.addEventListener("input", (event) => {
         foregroundColorPickerLabel.style.backgroundColor = event.target.value;
     });
     foregroundColorPicker.value = foregroundDefaultColor;
     foregroundColorPickerLabel.style.backgroundColor = foregroundDefaultColor;
+    backgroundColorPicker.addEventListener("input", (event) => {
+        backgroundColorPickerLabel.style.backgroundColor = event.target.value;
+    });
+    backgroundColorPicker.value = backgroundDefaultColor;
+    backgroundColorPickerLabel.style.backgroundColor = backgroundDefaultColor;
 }
 
 findElements();
